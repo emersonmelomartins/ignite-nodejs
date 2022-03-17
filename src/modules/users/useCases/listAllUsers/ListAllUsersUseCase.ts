762e9a61dbadd2bf061c652142e8795cb2d7ac1d
Emerson Melo Martins <emerson.melo@sap.trijay.com.br>
2022-03-16 22:00:16 -0300

    All tests passed

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,18 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User not found.");
+    }
+
+    if (!user.admin) {
+      throw new Error("You have no permission to continue this action.");
+    }
+
+    const users = this.usersRepository.list();
+    return users;
   }
 }
 
