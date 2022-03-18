2c64a8e0ef899d23cdc1e2483b6bc8152ad93d4b
Emerson Melo Martins <emerson.melo@sap.trijay.com.br>
2022-03-18 19:02:54 -0300

    Copied solid project

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+
+import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+
+class ListAllUsersController {
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    const user_id = String(request.headers.user_id);
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+      return response.status(200).json(users);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  }
+}
+
+export { ListAllUsersController };
